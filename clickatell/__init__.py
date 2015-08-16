1723d92187d787c3443b39f8f2daeb098a61a0d3
Chris Brand <dcstormrage@hotmail.com>
2015-08-16 20:17:53 +0200

    Bump package revision for next tag.

--- clickatell/__init__.py
+++ clickatell/__init__.py
@@ -119,7 +119,7 @@ class Transport:
         url = url + '/' + action
 
         # Set the User-Agent
-        userAgent = "".join(["ClickatellPython/0.1.1", " ", "Python/", platform.python_version()])
+        userAgent = "".join(["ClickatellPython/0.1.2", " ", "Python/", platform.python_version()])
         headers = self.merge({ "User-Agent": userAgent }, headers)
 
         try:
